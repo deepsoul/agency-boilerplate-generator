71a98ed7fea62000c9156c28930df82598d1a792
Boris Horn <boris.horn@grabarzundpartner.de>
2016-02-04 23:17:10 +0100

    bugfix yo now runs valid

--- generator-agency-boilerplate/generators/app/index.js
+++ generator-agency-boilerplate/generators/app/index.js
@@ -6,7 +6,6 @@ var yosay = require('yosay');
 var AgencyBoilerplateGenerator = yeoman.Base.extend({
 
 
-
   prompting: function() {
     var done = this.async();
     this.log(yosay(
@@ -16,15 +15,10 @@ var AgencyBoilerplateGenerator = yeoman.Base.extend({
     var prompts = [{
       name: 'name',
       message: 'What is your app\'s name ?'
-    },
-      {
-        name: 'destination',
-        message: 'What is your app\'s destination ?'
-      }];
+    }];
 
     this.prompt(prompts, function (props) {
       this.name = props.name;
-      this.destination = props.destination;
       done();
     }.bind(this));
 
@@ -33,78 +27,72 @@ var AgencyBoilerplateGenerator = yeoman.Base.extend({
     //Copy the configuration files
     config: function () {
       this.fs.copyTpl(
-          this.templatePath('_package.json'),
+          this.templatePath('package.json'),
           this.destinationPath('package.json'), {
             name: this.name
           }
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('.editorconfig'),
-          this.destinationPath('.editorconfig'), {
-            name: this.name
-          }
+          this.destinationPath('.editorconfig')
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('gulpfile.js'),
-          this.destinationPath('gulpfile.js'), {
-            name: this.name
-          }
+          this.destinationPath('gulpfile.js')
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('.jshintrc'),
-          this.destinationPath('.jshintrc'), {
-            name: this.name
-          }
+          this.destinationPath('.jshintrc')
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('.modernizrrc'),
-          this.destinationPath('.modernizrrc'), {
-            name: this.name
-          }
+          this.destinationPath('.modernizrrc')
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('.travis.yml'),
-          this.destinationPath('.travis.yml'), {
-            name: this.name
-          }
+          this.destinationPath('.travis.yml')
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('app.json'),
-          this.destinationPath('app.json'), {
-            name: this.name
-          }
+          this.destinationPath('app.json')
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('appveyor.yml'),
-          this.destinationPath('appveyor.yml'), {
-            name: this.name
-          }
+          this.destinationPath('appveyor.yml')
       );
-      this.fs.copyTpl(
-          this.templatePath('gulpfile.js'),
-          this.destinationPath('Procfile'), {
-            name: this.name
-          }
+      this.fs.copy(
+          this.templatePath('Procfile'),
+          this.destinationPath('Procfile')
       );
-      this.fs.copyTpl(
+      this.fs.copy(
           this.templatePath('README.md'),
-          this.destinationPath('README.md'), {
-            name: this.name
-          }
+          this.destinationPath('README.md')
       );
 
     },
     //Copy the enviroment files
     enviroment : function() {
-      this.directory(this.templatePath('_env'),this.destinationPath('env'), {destination: this.destination});
+      //this.directory(this.templatePath('_env'),this.destinationPath('env'), {destination: this.destination});
+        this.fs.copy(
+            this.templatePath('env/config/local.json'),
+            this.destinationPath('env/config/local.json'), {
+                name: this.name
+            }
+        );
+        this.fs.copy(
+            this.templatePath('env/config/tasks.json'),
+            this.destinationPath('env/config/tasks.json'), {
+                name: this.name
+            }
+        );
     },
     routes : function() {
-      this.directory(this.templatePath('_routes'),this.destinationPath('routes'));
+      this.directory(this.templatePath('env/routes'),this.destinationPath('env/routes'));
     },
     //Copy the src files
     src : function() {
 
-      this.directory(this.templatePath('_src'),this.destinationPath('src'));
+      this.directory(this.templatePath('src'),this.destinationPath('src'));
     }
   },
   install: function() {
