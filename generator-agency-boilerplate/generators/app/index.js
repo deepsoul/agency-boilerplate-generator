e7b440c4b02cf3d152624b8573eaecabb5511829
Boris Horn <boris.horn@grabarzundpartner.de>
2016-02-10 16:20:52 +0100

    fetch from github

--- generator-agency-boilerplate/generators/app/index.js
+++ generator-agency-boilerplate/generators/app/index.js
@@ -2,6 +2,7 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var extract = require('extract-zip');
 
 var AgencyBoilerplateGenerator = yeoman.Base.extend({
 
@@ -15,57 +16,99 @@ var AgencyBoilerplateGenerator = yeoman.Base.extend({
     var prompts = [{
       name: 'name',
       message: 'What is your app\'s name ?'
+    }, {
+      type: 'confirm',
+      name: 'git',
+      message: 'Would you like to init an empty git repo?',
+      default: true
     }];
 
     this.prompt(prompts, function (props) {
       this.name = props.name;
+      this.git = props.git;
       done();
     }.bind(this));
 
   },
   writing: {
+
+    init: function() {
+    var done = this.async();
+      if(this.git) {
+        this.spawnCommand('git', ['init']);
+        this.log(
+            chalk.magenta( 'git repo succesfully initialized' )
+        );
+        done();
+
+      }
+    },
+
+    src : function() {
+      var self = this;
+      var done = this.async();
+      this.fetch('https://github.com/StephanGerbeth/agency-boilerplate/archive/master.zip',
+          this.templatePath('git-zip'),
+          function() {
+            extract(self.templatePath('git-zip/master.zip'), {dir: self.templatePath('git-zip/unzipped')}, function (err) {
+              // extraction is complete. make sure to handle the err
+              if(err) {
+                self.log(yosay(
+                    'Oooops - there was an error!'
+                ));
+              }else {
+                self.directory(self.templatePath('git-zip/unzipped/agency-boilerplate-master/src'),self.destinationPath('src'));
+                done();
+              }
+            })
+          });
+    },
     //Copy the configuration files
     config: function () {
       this.fs.copyTpl(
-          this.templatePath('package.json'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/package.json'),
           this.destinationPath('package.json'), {
             name: this.name
           }
       );
       this.fs.copy(
-          this.templatePath('.editorconfig'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/.editorconfig'),
           this.destinationPath('.editorconfig')
       );
       this.fs.copy(
-          this.templatePath('gulpfile.js'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/.gitignore'),
+          this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/gulpfile.js'),
           this.destinationPath('gulpfile.js')
       );
       this.fs.copy(
-          this.templatePath('.jshintrc'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/.jshintrc'),
           this.destinationPath('.jshintrc')
       );
       this.fs.copy(
-          this.templatePath('.modernizrrc'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/.modernizrrc'),
           this.destinationPath('.modernizrrc')
       );
       this.fs.copy(
-          this.templatePath('.travis.yml'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/.travis.yml'),
           this.destinationPath('.travis.yml')
       );
       this.fs.copy(
-          this.templatePath('app.json'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/app.json'),
           this.destinationPath('app.json')
       );
       this.fs.copy(
-          this.templatePath('appveyor.yml'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/appveyor.yml'),
           this.destinationPath('appveyor.yml')
       );
       this.fs.copy(
-          this.templatePath('Procfile'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/Procfile'),
           this.destinationPath('Procfile')
       );
       this.fs.copy(
-          this.templatePath('README.md'),
+          this.templatePath('git-zip/unzipped/agency-boilerplate-master/README.md'),
           this.destinationPath('README.md')
       );
 
@@ -74,31 +117,31 @@ var AgencyBoilerplateGenerator = yeoman.Base.extend({
     enviroment : function() {
       //this.directory(this.templatePath('_env'),this.destinationPath('env'), {destination: this.destination});
         this.fs.copy(
-            this.templatePath('env/config/local.json'),
+            this.templatePath('git-zip/unzipped/agency-boilerplate-master/env/config/local.json'),
             this.destinationPath('env/config/local.json')
         );
         this.fs.copy(
-            this.templatePath('env/config/tasks.json'),
+            this.templatePath('git-zip/unzipped/agency-boilerplate-master/env/config/tasks.json'),
             this.destinationPath('env/config/tasks.json'), {
                 name: this.name
             }
         );
     },
     routes : function() {
-      this.directory(this.templatePath('env/routes'),this.destinationPath('env/routes'));
+      this.directory(this.templatePath('git-zip/unzipped/agency-boilerplate-master/env/routes'),this.destinationPath('env/routes'));
     },
-    //Copy the src files
-    src : function() {
 
-      this.directory(this.templatePath('src'),this.destinationPath('src'));
-    }
   },
   install: function() {
+
+    this.log(
+        chalk.magenta( 'Install npm packages - Takes a bit ;-) ')
+    );
     this.npmInstall();
   },
   end: function () {
     this.log(yosay(
-        'It\'s all done!'
+        'It\'s all done! Happy Coding'
     ));
   }
 
--- generator-agency-boilerplate/generators/app/templates/src/js/packages.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-    {name: "./partials/components/Header", id: require.resolve("./partials/components/Header"), controller: require("./partials/components/Header")},
-    {name: "./partials/components/DragDrop", id: require.resolve("./partials/components/DragDrop"), controller: require("./partials/components/DragDrop")},
-    {name: "./partials/components/ParallaxContainer", id: require.resolve("./partials/components/ParallaxContainer"), controller: require("./partials/components/ParallaxContainer")},
-    {name: "./partials/components/Animation", id: require.resolve("./partials/components/Animation"), controller: require("./partials/components/Animation")},
-    {name: "./partials/common/AdBlocker", id: require.resolve("./partials/common/AdBlocker"), controller: require("./partials/common/AdBlocker")},
-    {name: "./partials/common/OutdatedBrowser", id: require.resolve("./partials/common/OutdatedBrowser"), controller: require("./partials/common/OutdatedBrowser")}
-];
